06b3df5d8f2598c5bbb924dded83a7df653f2845
VArtemenko <artemenko.vladimi@gmail.com>
2015-10-05 19:59:20 +0300

    Refactoring project

--- Entity/BinaryTree.cs
+++ Entity/BinaryTree.cs
@@ -4,10 +4,41 @@ namespace Entity
 {
 	public class BinaryTree<T> where T : IComparable
 	{
-		private TreeItem<T> head { get; set; }
+		#region BinaryTreeNode
 
-		public void Add(TreeItem<T> item) 
+		public class BinaryTreeNode : IComparable
 		{
+			public T data { get; set; }
+			public BinaryTreeNode left { get; set; }
+			public BinaryTreeNode right { get; set; }
+
+			public BinaryTreeNode (T data)
+			{
+				this.data = data;
+			}
+
+			#region IComparable implementation
+
+			public int CompareTo (object obj)
+			{
+				if (obj == null)
+					return -1;
+
+				var objBinaryTreeNode = obj as BinaryTreeNode;
+				return this.data.CompareTo (objBinaryTreeNode.data);
+			}
+
+			#endregion
+		}
+
+		#endregion
+
+		public BinaryTreeNode head { get; private set; }
+		public int Capasity { get; private set; }
+
+		public void Add(T value) 
+		{
+			var item = new BinaryTreeNode (value);
 			if (item == null)
 				return;
 
@@ -24,6 +55,7 @@ namespace Entity
 				if (tmp.CompareTo (item) < 0) {
 					if (tmp.left == null) {
 						tmp.left = item;
+						Capasity++;
 						return;
 					}
 					tmp = tmp.left;
@@ -31,6 +63,7 @@ namespace Entity
 				} else {
 					if (tmp.right == null) {
 						tmp.right = item;
+						Capasity++;
 						return;
 					}
 					tmp = tmp.right;
